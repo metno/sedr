faeca5a5ee703c8530629039c74303e234db66bd
Lars Falk-Petersen <larsfp@met.no>
2024-09-26 13:39:03 +0200

    Add unit tests for init

---
+++ sedr/test_init.py
@@ -0,0 +1,23 @@
+"""Unit tests for __init__.py."""
+
+import unittest
+import subprocess
+import sys
+
+class TestInit(unittest.TestCase):
+
+    def test_version(self):
+        result = subprocess.run([sys.executable, "./__init__.py", "--version"], capture_output=True, text=True)
+        self.assertIn("v", result.stdout)
+
+    def test_help(self):
+        result = subprocess.run([sys.executable, "./__init__.py", "--help"], capture_output=True, text=True)
+        self.assertIn("usage:", result.stdout)
+
+    def test_run(self):
+        result = subprocess.run([sys.executable, "./__init__.py", "--url", "https://edrisobaric.k8s.met.no"], capture_output=True, text=True)
+        self.assertEqual(0, result.returncode)
+
+
+if __name__ == "__main__":
+    unittest.main()
