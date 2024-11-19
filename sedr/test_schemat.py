dfee92d5cbf2a82c5a93628497f91e21d602fd14
Lars Falk-Petersen <larsfp@met.no>
2024-11-19 15:03:41 +0100

    Comment out a non-working test

--- sedr/test_schemat.py
+++ sedr/test_schemat.py
@@ -14,9 +14,27 @@ class TestInit(unittest.TestCase):
 
         util.logger = util.set_up_logging(
            args=util.args, logfile=util.args.log_file, version=__version__
-)
+        )
         import schemat
 
         schemat.schema = schemat.set_up_schemathesis(util.args)
         self.assertTrue(schemat.schema)
 
+
+    # def test_edr_landingpage(self):
+    #     """Test test_edr_landingpage."""
+    #     __version__ = "testversion"
+
+    #     util.args = util.parse_args([], __version__)
+    #     util.args.openapi_version == "3.1"
+
+    #     util.logger = util.set_up_logging(
+    #        args=util.args, logfile=util.args.log_file, version=__version__
+    #     )
+    #     import schemat
+
+    #     schemat.schema = schemat.set_up_schemathesis(util.args)
+
+    # TODO: Need to set up a case to run this
+    #     t = schemat.test_edr_landingpage()
+    #     self.assertTrue(t)
