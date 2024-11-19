93641c11967f499e72704426acb25c19b110801f
Lars Falk-Petersen <larsfp@met.no>
2024-11-19 16:33:20 +0100

    Remove commented code

--- sedr/test_util.py
+++ sedr/test_util.py
@@ -12,10 +12,3 @@ class TestUtil(unittest.TestCase):
             landingpage_json = json.loads(f.read())
         landing, _ = util.parse_landing_json(landingpage_json)
         self.assertTrue(landing)
-
-    # def test_parse_bad_landing(self):
-    #     """Test parsing a bad landing page (in json)."""
-    #     with open("testdata/landingpage_bad_service-desc.json", "r", encoding="utf-8") as f:
-    #         landingpage_json = json.loads(f.read())
-    #     landing, _ = util.parse_landing_json(landingpage_json)
-    #     self.assertFalse(landing)
