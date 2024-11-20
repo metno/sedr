0f2539467ab2bfb73c7bdaa1589e985566c5ac73
Lars Falk-Petersen <larsfp@met.no>
2024-11-20 10:44:32 +0100

    Remove incomplete test

--- sedr/test_schemat.py
+++ sedr/test_schemat.py
@@ -16,27 +16,5 @@ class TestInit(unittest.TestCase):
             args=util.args, logfile=util.args.log_file, version=__version__
         )
         import schemat
-
         schemat.schema = schemat.set_up_schemathesis(util.args)
         self.assertTrue(schemat.schema)
-
-    def test_edr_landingpage(self):
-        """Test test_edr_landingpage with a local json."""
-        __version__ = "testversion"
-
-        util.args = util.parse_args([], __version__)
-        util.args.openapi_version == "3.1"
-
-        util.logger = util.set_up_logging(
-            args=util.args, logfile=util.args.log_file, version=__version__
-        )
-        import schemathesis
-        import schemat
-
-        schemat.schema = schemathesis.from_path(
-            path="testdata/edrisobaric_openapi.json"
-        )
-
-        # TODO
-        # t = schemat.test_edr_landingpage(schemat.schema)
-        # self.assertTrue(t)
