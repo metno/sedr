f4f6e7dc261f00da45fb42aba761d3f6101bb15e
Lars Falk-Petersen <larsfp@met.no>
2024-11-25 10:09:11 +0100

    Move more checks from schemathesis to preflight.

--- sedr/preflight.py
+++ sedr/preflight.py
@@ -3,6 +3,7 @@
 import sys
 import util
 import requests
+from urllib.parse import urljoin
 import edreq11 as edreq
 import rodeoprofile10 as rodeoprofile
 
@@ -16,34 +17,101 @@ def test_site_response(url: str, timeout=10) -> bool:
     return True
 
 
-def parse_landingpage(url, timeout=10) -> bool:
+def parse_landing(url, timeout=10) -> bool:
     """Test that the landing page contains required elements."""
-    landingpage_json = None
-    response = requests.get(util.args.url, timeout=timeout)
+    landing_json = None
+    response = requests.get(url, timeout=timeout)
 
     try:
-        landingpage_json = response.json()
+        landing_json = response.json()
     except json.decoder.JSONDecodeError:
-        util.logger.warning("Landing page is not valid JSON.")
+        util.logger.warning("Landing page <%s> is not valid JSON.", url)
         return False
 
-    landing, requirement9_1_message = edreq.requirement9_1(landingpage_json)
+    landing, requirement9_1_message = edreq.requirement9_1(landing_json)
     if not landing:
         util.logger.error(requirement9_1_message)
         return False
 
-    if util.args.rodeo_profile:
+    requirementA2_2_A7, requirementA2_2_A7_message = edreq.requirementA2_2_A7(
+        response.raw.version
+    )
+    if not requirementA2_2_A7:
+        util.logger.error(requirementA2_2_A7_message)
+        return False
+
+    resolves, resolves_message = util.test_conformance_links(
+        jsondata=landing_json
+    )
+    if not resolves:
+        util.logger.error(resolves_message)
+        return False
+
+    return True, landing_json
+
+
+def parse_conformance(url: str, timeout=10, landing_json={}) -> bool:
+    """Test that the conformance page contains required elements."""
+    conformance_json = None
+    response = requests.get(url, timeout=timeout)
+
+    try:
+        conformance_json = response.json()
+    except json.decoder.JSONDecodeError:
+        util.logger.warning("Conformance page <%s> is not valid JSON.", url)
+        return False
+
+    requirementA2_2_A5, requirementA2_2_A5_message = edreq.requirementA2_2_A5(
+        jsondata=conformance_json, siteurl=util.args.url
+    )
+    if not requirementA2_2_A5:
+        util.logger.error(requirementA2_2_A5_message)
+        return False
+
+    requirementA11_1, requirementA11_1_message = edreq.requirementA11_1(
+        jsondata=conformance_json
+    )
+    if not requirementA11_1:
+        util.logger.error(requirementA11_1_message)
+        return False
+
+    # Rodeo profile
+
+    if util.args.rodeo_profile or rodeoprofile.conformance_url in conformance_json["conformsTo"]:
+        util.logger.info(
+            "Including tests for Rodeo profile %s", rodeoprofile.conformance_url
+        )
+
         requirement7_2, requirement7_2_message = rodeoprofile.requirement7_2(
-            jsondata=landingpage_json
+            jsondata=landing_json
         )
         if not requirement7_2:
             util.logger.error(requirement7_2_message)
             return False
+
+
+        requirement7_1, requirement7_1_message = rodeoprofile.requirement7_1(
+            jsondata=conformance_json
+        )
+        if not requirement7_1:
+            util.logger.error(requirement7_1_message)
+            return False
+
     return True
 
 
 def main():
-    if test_site_response(util.args.url, util.args.timeout) and \
-        parse_landingpage(util.args.url, util.args.timeout):
-        return True
-    return False
+    conformance_url = urljoin(util.args.url, "/conformance")
+
+    if not test_site_response(util.args.url, util.args.timeout):
+        return False
+
+    landing_ok, landing_json = parse_landing(util.args.url, util.args.timeout)
+    if not landing_ok:
+        return False
+
+    if not parse_conformance(conformance_url, util.args.timeout, landing_json):
+        return False
+
+    util.logger.info("Preflight checks passed.")
+    return True
