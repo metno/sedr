8aabede090586a403a196a0a163474d543180825
Lars Falk-Petersen <larsfp@met.no>
2024-11-29 10:27:07 +0100

    Add tests for ogcapi10

--- sedr/test_ogcapi10.py
+++ sedr/test_ogcapi10.py
@@ -1,33 +1,33 @@
-"""Unit tests for edreq12.py."""
+"""Unit tests for ogcapi10.py."""
 
 import unittest
 import json
 import util
-import edreq12 as edreq
+import ogcapi10 as ogcapi
 
 
-class TestEDR(unittest.TestCase):
+class TestOGCAPI(unittest.TestCase):
     __version__ = "testversion"
     util.args = util.parse_args(["--url", "https://example.com/"], __version__)
     util.logger = util.set_up_logging(
         args=util.args, logfile=util.args.log_file, version=__version__
     )
 
-    def test_requrementA5_2(self):
+    def test_requirement9_1(self):
         # Good tests
         jsondata = {}
-        with open("testdata/edrisobaric_collection.json", "r", encoding="utf-8") as f:
+        with open("testdata/edrisobaric_landing.json", "r", encoding="utf-8") as f:
             jsondata = json.load(f)
-        ok, _ = edreq.requrementA5_2(jsondata)
+        ok, _ = ogcapi.requirement9_1(jsondata)
         self.assertTrue(ok)
 
         # Bad tests
         jsondata = {}
         with open(
-            "testdata/edrisobaric_collection_bad_bbox.json", "r", encoding="utf-8"
+            "testdata/edrisobaric_landing-bad-desc.json", "r", encoding="utf-8"
         ) as f:
             jsondata = json.load(f)
-        ok, _ = edreq.requrementA5_2(jsondata)
+        ok, _ = ogcapi.requirement9_1(jsondata)
         self.assertFalse(ok)
 
 
