f7378c2034cd0193b0cc0a88a316a8e0dd6fbdb4
Lars Falk-Petersen <larsfp@met.no>
2025-04-24 12:53:35 +0200

    Tag new version

--- sedr/__init__.py
+++ sedr/__init__.py
@@ -1,7 +1,7 @@
 """Main init file for sedr. Schemathesis related in schemat.py, others in utils.py."""
 
 __license__ = "GPL-2.0"
-__version__ = "v0.9.2"
+__version__ = "v0.10.0"
 
 import sys
 import pytest
