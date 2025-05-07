558960893090b5b7fbf8288b760802399356f709
Håvard Futsæter <havardf@met.no>
2025-05-07 16:44:26 +0200

    Spec url ref for req 7.11 and 7.12. Restructured 7.11 test.

--- sedr/rodeoprofilecore10.py
+++ sedr/rodeoprofilecore10.py
@@ -409,44 +409,53 @@ def requirement7_11(jsondata: dict) -> tuple[bool, str]:
     Metadata about the radius data query in a collection.
     """
 
-    if "radius" in jsondata["data_queries"]:
-        # A
-        try:
-            if (
+    spec_url = f"{spec_base_url}#_collection_radius_data_query"
+
+    if "radius" not in jsondata["data_queries"]:
+        return (
+            True,
+            "Collection has no radius query.",
+        )
+
+    # A
+    try:
+        if (
+            "within_units"
+            not in jsondata["data_queries"]["radius"]["link"]["variables"]
+            or "m"
+            not in jsondata["data_queries"]["radius"]["link"]["variables"][
                 "within_units"
-                not in jsondata["data_queries"]["radius"]["link"]["variables"]
-                or "m"
-                not in jsondata["data_queries"]["radius"]["link"]["variables"][
-                    "within_units"
-                ]
-            ):
-                return (
-                    False,
-                    "The value 'm' SHALL be included in the within_units array in data_queries.radius.link.variables.",
-                )
-        except KeyError as error:
+            ]
+        ):
             return (
                 False,
-                f"Collection radius metadata is missing required properties: {error}.",
+                "The value 'm' SHALL be included in the within_units array in data_queries.radius.link.variables. See "
+                f"<{spec_url}> for more info.",
             )
-        # B
-        ureg = pint.UnitRegistry()
-        for unit in jsondata["data_queries"]["radius"]["link"]["variables"][
-            "within_units"
-        ]:
-            try:
-                # Check if the unit is a valid length unit
-                parsed_unit = ureg.Unit(unit)
-                if parsed_unit.dimensionality != ureg.m.dimensionality:
-                    return (
-                        False,
-                        f"Value '{unit}' in within_units is not a valid length unit.",
-                    )
-            except pint.UndefinedUnitError:
+    except KeyError as error:
+        return (
+            False,
+            f"Collection radius metadata is missing required properties: {error}. See "
+            f"<{spec_url}> for more info.",
+        )
+    # B
+    ureg = pint.UnitRegistry()
+    for unit in jsondata["data_queries"]["radius"]["link"]["variables"]["within_units"]:
+        try:
+            # Check if the unit is a valid length unit
+            parsed_unit = ureg.Unit(unit)
+            if parsed_unit.dimensionality != ureg.m.dimensionality:
                 return (
                     False,
-                    f"Value '{unit}' in within_units is not a recognized unit.",
+                    f"Value '{unit}' in within_units is not a valid length unit."
+                    f"See <{spec_url}> for more info.",
                 )
+        except pint.UndefinedUnitError:
+            return (
+                False,
+                f"Value '{unit}' in within_units is not a recognized unit. See "
+                f"<{spec_url}> for more info.",
+            )
 
     return (
         True,
@@ -462,32 +471,37 @@ def requirement7_12(resp: requests.Response) -> tuple[bool, str]:
 
     Structure of the response document for a /locations query.
     """
+    spec_url = f"{spec_base_url}#_locations_query_response_format"
 
     if resp.headers["Content-Type"] != "application/geo+json":
         return (
             False,
-            "The response document for a /locations query SHALL have Content-Type set to 'application/geo+json'.",
+            "The response document for a /locations query SHALL have Content-Type set to 'application/geo+json'. See "
+            f"<{spec_url}> for more info.",
         )
 
     jsondata = resp.json()
     if "type" not in jsondata or jsondata["type"] != "FeatureCollection":
         return (
             False,
-            "The response document for a /locations query SHALL have type set to 'FeatureCollection'.",
+            "The response document for a /locations query SHALL have type set to 'FeatureCollection'. See "
+            f"<{spec_url}> for more info.",
         )
 
     for feature in jsondata["features"]:
         if "id" not in feature or not isinstance(feature["id"], str):
             return (
                 False,
-                "The response document for a /locations query SHALL have an id of type string in each feature.",
+                "The response document for a /locations query SHALL have an id of type string in each feature. See "
+                f"<{spec_url}> for more info.",
             )
         if "name" not in feature["properties"] or not isinstance(
             feature["properties"]["name"], str
         ):
             return (
                 False,
-                "The response document for a /locations query SHALL have a name in each feature.properties.",
+                "The response document for a /locations query SHALL have a name in each feature.properties. See "
+                f"<{spec_url}> for more info.",
             )
 
     return (
