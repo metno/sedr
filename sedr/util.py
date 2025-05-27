4e106ff992e28194fb7eb669a70a37dea10c105a
Lars Falk-Petersen <larsfp@met.no>
2025-05-27 13:23:23 +0200

    Set rodeo-profile-core to default false

--- sedr/util.py
+++ sedr/util.py
@@ -50,8 +50,8 @@ def parse_args(args, version: str = "") -> argparse.Namespace:
     parser.add_argument(
         "--rodeo-profile-core",
         action="store_true",
-        default=True,
-        help="Use the rodeo profile core conformance class even though the API doesn't specify it. Default True.",
+        default=False,
+        help="Use the rodeo profile core conformance class even though the API doesn't specify it. Default False.",
     )
     parser.add_argument(
         "--rodeo-profile-insitu-observations",
